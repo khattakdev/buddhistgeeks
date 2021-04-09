d077f9d724f6567f92f0c53c0f83f02e91a8ee7c
celine <celinepark94@gmail.com>
2021-04-09 17:15:02 -0400

    add 404 image

--- pages/404.ts
+++ pages/404.ts
@@ -3,14 +3,24 @@ import Link from 'next/link'
 
 import {Box} from 'components/Layout'
 import { useUserData } from 'src/data'
+import styled from '@emotion/styled'
 
 export default ()=>{
   let {data: user} = useUserData()
 
   return h(Box, {style: {textAlign: 'center', width: 'fit-content', margin: 'auto'}}, [
-    h('h1', '404'),
-    h('p', "This page doesn't exist :("),
-    h('p', "It could be bad link, or maybe we're just still working on it!"),
-    h(Link, {href: user ? '/dashboard' : '/'}, h('a.mono', 'take me back to the homepage'))
+    h('h2', "This page doesn't exist :("),
+    h('h3', "It could be bad link, or maybe we're still working on it!"),
+    h(Link, {href: user ? '/dashboard' : '/'}, 
+      h('a', [h('h3', 'Go back home!')])
+    ),
+    h(FourOFourImg, {src:'/img/404.gif'}),
+
   ])
 }
+
+
+const FourOFourImg = styled('img') `
+  max-width: 100%;
+  margin: auto auto;
+`
