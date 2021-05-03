203340ec0cb69f06fb642fd681ed3e9b226d1f41
Arsalan Khattak <me@khattak.dev>
2021-05-03 22:30:03 +0500

    update anonymous function

--- components/NewsletterSignup.ts
+++ components/NewsletterSignup.ts
@@ -1,33 +1,44 @@
-import h from 'react-hyperscript'
-import { useState } from 'react'
-import { useApi } from '../src/apiHelpers'
-import { NewsletterSignupMsg, NewsletterSignupResponse } from '../pages/api/signup/[action]'
-import { LabelBox, FormBox, Box } from './Layout'
-import { Input } from './Form'
-import { Secondary } from './Button'
+import h from "react-hyperscript";
+import { useState } from "react";
+import { useApi } from "../src/apiHelpers";
+import {
+  NewsletterSignupMsg,
+  NewsletterSignupResponse,
+} from "../pages/api/signup/[action]";
+import { LabelBox, FormBox, Box } from "./Layout";
+import { Input } from "./Form";
+import { Secondary } from "./Button";
 
-export default () => {
-	let [email, setEmail] = useState('')
-  let [status, callNewsletterSignup] = useApi<NewsletterSignupMsg, NewsletterSignupResponse>([email])
+const NewsLetter = () => {
+  let [email, setEmail] = useState("");
+  let [status, callNewsletterSignup] = useApi<
+    NewsletterSignupMsg,
+    NewsletterSignupResponse
+  >([email]);
 
-  let onSubmit = (e: React.FormEvent)=>{
-    e.preventDefault()
-    callNewsletterSignup('/api/signup/newsletter',{email})
-  }
+  let onSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    callNewsletterSignup("/api/signup/newsletter", { email });
+  };
 
-  return h(FormBox, {onSubmit, gap: 16, style:{maxWidth: 320}}, [
-    h(LabelBox, {gap:8},[
-      h(Box, {gap:4}, [
-        h('h4', "Drop your email to get updates about new courses and more!"),
-        h('small', "We'll never spam or share your email. You can unsubscribe at any time."),
+  return h(FormBox, { onSubmit, gap: 16, style: { maxWidth: 320 } }, [
+    h(LabelBox, { gap: 8 }, [
+      h(Box, { gap: 4 }, [
+        h("h4", "Drop your email to get updates about new courses and more!"),
+        h(
+          "small",
+          "We'll never spam or share your email. You can unsubscribe at any time."
+        ),
       ]),
       h(Input, {
         placeholder: "Your email",
         type: "email",
         value: email,
-        onChange: e => setEmail(e.currentTarget.value)
+        onChange: (e) => setEmail(e.currentTarget.value),
       }),
     ]),
-    h(Secondary, {type: "submit", status}, 'Get Updates'),
-  ])
-}
+    h(Secondary, { type: "submit", status }, "Get Updates"),
+  ]);
+};
+
+export default NewsLetter;
