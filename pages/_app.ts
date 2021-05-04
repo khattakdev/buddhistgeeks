d30a9bb90fe13f719c69c935350edf6ae1308a6c
Arsalan Khattak <37709578+khattakdev@users.noreply.github.com>
2021-05-04 16:07:36 +0500

    update head (#2)

--- pages/_app.ts
+++ pages/_app.ts
@@ -1,31 +1,44 @@
-import h from 'react-hyperscript'
-import Head from 'next/head'
-import Layout from '../components/Layout';
-import * as Sentry from '@sentry/node'
-import { Fragment } from 'react';
+import h from "react-hyperscript";
+import Head from "next/head";
+import Layout from "../components/Layout";
+import * as Sentry from "@sentry/node";
+import { Fragment } from "react";
 
 if (process.env.NEXT_PUBLIC_SENTRY_DSN) {
   Sentry.init({
-    enabled: process.env.NODE_ENV === 'production',
+    enabled: process.env.NODE_ENV === "production",
     dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
-  })
+  });
 }
 
 type Props = {
-  Component: any,
-  pageProps: any
-}
+  Component: any;
+  pageProps: any;
+};
 
-const App = ({ Component, pageProps}:Props) => {
+const App = ({ Component, pageProps }: Props) => {
   return h(Fragment, [
-    h(Head as React.StatelessComponent, [
-      h('title', 'hyperlink.academy'),
-      h('meta', {property:"og:title", content:'hyperlink.academy', key:"og:title"}),
-      h('meta', {property:"og:description", content:'a course platform and online school built for seriously effective learning', key:"og:description"}),
-      h('meta', {property: "og:image", content: 'https://hyperlink.academy/img/social-logo.png', key: "og:image"}),
+    h(Head as React.FunctionComponent, [
+      h("title", "buddhistgeeks.training"),
+      h("meta", {
+        property: "og:title",
+        content: "buddhistgeeks.training",
+        key: "og:title",
+      }),
+      h("meta", {
+        property: "og:description",
+        content:
+          "a course platform and online school built for seriously effective learning",
+        key: "og:description",
+      }),
+      h("meta", {
+        property: "og:image",
+        content: "http://localhost:3000/img/social-logo.png", // This is temporary
+        key: "og:image",
+      }),
     ]),
-    h(Layout, {}, [h(Component, {...pageProps})])
-  ])
-}
+    h(Layout, {}, [h(Component, { ...pageProps })]),
+  ]);
+};
 
-export default App
+export default App;
